64d0fd4e32fe73fef743373981b7b645b9948584
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-04-01 19:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("1696386360:AAFMPYEKS5tdF7vY9UUrbyaPcsKEJq5-TNY", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("3454845", 12345))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("a205de875f47541dfa6b213bdfe56d36", "")
+
+    CHAT_GROUPS = os.environ.get("1001239208195", "")
+
+    OWNER_ID = os.environ.get("1650092910", "")
